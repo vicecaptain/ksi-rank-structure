4c5f56136ae2e251b0a851c93dddf34eb3625475
vicecaptain <matt.lo@live.com>
2013-05-02 22:23:35 -0500

    fixed reference path

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<title>KSI Rank Structure</title>
 		<meta charset="utf-8" />
 		<link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet" type="text/css" />
-		<link href="/css/app.css" rel="stylesheet" type="text/css" />
+		<link href="css/app.css" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
 		<header>
@@ -29,6 +29,6 @@
 		</footer>
 		
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-		<script src="/js/app.js"></script>
+		<script src="js/app.js"></script>
 	</body>
 </html>
