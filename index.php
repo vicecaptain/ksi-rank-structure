f13ebd4b2da1770689ea746f188de17fee953e8e
vicecaptain <matt.lo@live.com>
2013-05-03 02:25:58 -0500

    added nav UI

--- index.php
+++ index.php
@@ -16,6 +16,14 @@ include "app.php";
 		$dataLength = count($data);
 		?>
 		
+		<div class="sticky-nav">
+			<div>
+				<a class="btn btn-left" href=""><span><i class="icon-circle-arrow-left"></i> Previous Rank</span></a>
+				<a class="btn btn-right" href=""><span>Next Rank <i class="icon-circle-arrow-right"></i></span></a>
+				<span class="rank-output"><span class="data-out"></span></span>
+			</div>
+		</div>
+		
 		<header>
 			<h1>KSI Hierarchy Ranking Structure</h1>
 			<h2>From <strong><?php echo $data[$dataLength - 1]->getName(); ?></strong> To <strong><?php echo $data[0]->getName(); ?></strong></h2>
@@ -25,7 +33,13 @@ include "app.php";
 			<ul>
 				<?php foreach($data as $index => $rank) { ?>
 					<li>
-						<h3><span>#<?php echo $dataLength - $index; ?> <?php echo $rank->getName(); ?> <i class="icon-user"></i></span></h3>
+						<h3>
+							<span class="block">
+								<span class="pound-sign">#<?php echo $dataLength - $index; ?></span>
+								<span id="goto-<?php echo $index; ?>" class="rank-place"><?php echo $rank->getName(); ?></span>
+								<i class="icon-user"></i>
+							</span>
+						</h3>
 						<p>
 							<?php echo $rank->getDescription(); ?>
 						</p>
@@ -34,10 +48,41 @@ include "app.php";
 			</ul>
 		</section>
 		
+		
+		<section class="about">
+			<h2>About KSI</h2>
+			<p>
+				The KSI Global Gaming Network is the cliché, yet unique result of three men who sat down and planned to begin the greatest gaming community ever produced. 
+				Consisting of a special group of young and older individuals that share a common goal bridging the gap, and building leaders for every age group to reinforce their real life work ethics, and goal oriented lifestyles. 
+				The cultures here in KSI are limitless, producing leaders from every country, and every language. 
+				The members are taught diversity, and live that belief to the fullest, making KSI one of the most diverse communities in the world. 
+			</p>
+			<p>
+				KSI’s leaders live the life in becoming more proficient with their knowledge strength and integrity. 
+				KSI’s members can experiment with players from across the world, to meet leaders from every walk of life. 
+				The members can learn certain traits from their leaders to mold themselves, producing a fine role-model for future KSI members, and then expressing what they learned while online, and in their workplace or at school. 
+				KSI is a learning environment and reinforces their drive, making its members more confident in their leadership, and less reckless with real life relationships. 
+			</p>
+			<p>
+				KSI within the first few months consisted of only 600 members, but now that number has reached a crushing number of 20000.
+				KSI has also integrated a new method of adding divisions and ramifying the divisions into squads. 
+				Divisions are very unique with a hierarchy of a few key individuals who assist the members in staying organized and making great changes in the good of its members and the division. 
+				A division may reach around 1000 members but may be much more. 
+				The members are also placed in a squad where they learn everything they need to know, from organizational skills to politics, and even as far as personal relationships. 
+				The members, during game play are encouraged to be competitive but are reminded of good sportsmanship and team building concepts. 
+			</p>
+			<p>
+				KSI has members that range from the age of 15-59, where the younger gamers will interact with older gamers, 
+				giving them the opportunity to learn to play more maturely, and getting older gamers to interact with the youth, along the way, teaching valuable lessons, and a wide variety of social skills.
+				KSI's older members are given a chance to have fun, and give the XBOX LIVE community a much more organized feel. 
+				In KSI these members utilize this point in KSI to have the very unique experience of building a global gaming community.
+			</p>
+		</section>
+		
 		<footer>
 			<div>
 				<div>Created by <strong>KSI ViceCaptain</strong> from the KSI Global Gaming Network.</div>
-				Download or contribute to the source at <a href="https://github.com/vicecaptain/ksi-rank-structure" target="_blank"><i class="icon-github-alt"></i> https://github.com/vicecaptain/ksi-rank-structure</a>
+				Download or contribute to the source at <a href="https://github.com/vicecaptain/ksi-rank-structure" target="_blank"><i class="icon-github-alt"></i> https://github.com/vicecaptain/ksi-rank-structure</a>.
 			</div>
 		</footer>
 		
--- js/app.js
+++ js/app.js
@@ -1,3 +1,87 @@
-$(document).ready(function () {
+var Ranks = {
+	data: [],
+	current: 0,
+	register: function (name, min, max, id) {
+		this.data.push({
+			name: name,
+			min: min,
+			max: max,
+			id: id
+		});
+	},
+	rankAtPosition: function (x) {
+		var i;
+		
+		for (i = 0; i < this.data.length; ++i) {
+			if (this.data[i].min <= x && x <= this.data[i].max) {
+				this.current = i;
+				return this.data[i];
+			}
+		}
+		
+		return '';
+	},
+	getData: function () {
+		return this.data;
+	},
+	getCurrent: function () {
+		return this.current;
+	},
+	setCurrent: function (i) {
+		this.current = i;
+	},
+	toggleButtons: function () {
+		if (this.current === 0) {
+			$('.btn-left').hide();
+		} else {
+			$('.btn-left').css({display: 'inline-block'});
+		}
+		
+		if (this.current === this.data.length - 1) {
+			$('.btn-right').hide();
+		} else {
+			$('.btn-right').css({display: 'inline-block'});
+		}
+	}
+};
 
+$(window).load(function () {
+	// map data
+	var lastPosition = 0;
+	$('.rank-place').each(function () {
+		Ranks.register($(this).text(), lastPosition, $(this).position().top, this.id);
+		lastPosition = $(this).position().top + 1;
+	});
+	
+	console.log(Ranks.getData());
+	
+	$(window).scroll(function () {
+		updateRankDisplay();
+	});
+	
+	updateRankDisplay();
 });
+
+$(document).ready(function () {
+	$('.btn-left').click(function (e) {
+		e.preventDefault();
+		
+		var node = $('#' + Ranks.getData()[Ranks.getCurrent()-1].id);
+		
+		$('html, body').stop(true).animate({scrollTop: node.position().top - 50});
+	});
+	
+	$('.btn-right').click(function (e) {
+		e.preventDefault();
+		
+		var node = $('#' + Ranks.getData()[Ranks.getCurrent()+1].id);
+		
+		$('html, body').stop(true).animate({scrollTop: node.position().top - 50});
+	});
+});
+
+function updateRankDisplay() {
+	var r = Ranks.rankAtPosition($(window).scrollTop());
+	$('.data-out').text(r.name);
+	Ranks.toggleButtons();
+}
